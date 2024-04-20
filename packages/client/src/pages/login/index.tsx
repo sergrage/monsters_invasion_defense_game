0711e6159f8bd03a312296b0550ac4e04b958714
Julius Gerba <juliusgerba@gmail.com>
2024-04-20 09:44:00 +0300

    feat: add auth form, endpoint

--- packages/client/src/pages/login/index.tsx
+++ packages/client/src/pages/login/index.tsx
@@ -1,40 +1,92 @@
-import React, { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 
-import style from "./style.module.scss";
-import Title from "@/ui/title";
 import { useNavigate } from "react-router";
 import { routes } from "@/pages/routes";
-import Layout from "@/components/Layout";
+import { authUrl } from "@/endpoints/apiUrl";
 
-const LoginPage: FC = () => {
+import useFetch from "@/hooks/useFetch";
+
+import FlatButton from "@/ui/flat-button";
+import ExoticInput from "@/ui/exotic-input";
+
+import style from "./style.module.scss";
+
+function LoginPage() {
   const navigate = useNavigate();
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-    };
+  const sendRequest = useFetch();
 
-    fetchServerData();
+  const [formVal, setFormVal] = useState({ login: "", password: "" });
+  const [hasError, setHasError] = useState(false);
+
+  // при первой отрисовке если позволяют куки то получаем данные юзера с дальнейшим редиректом
+  useEffect(() => {
+    applyData();
   }, []);
 
-  const onClickHandler = () => {
+  function onClickHandler() {
     navigate(routes.signup);
-  };
-  return (
-    <Layout.Page>
-      <Title.H2 title={"Страница авторизации"} />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
-        architecto consectetur culpa dicta dignissimos dolorem eius eos fugiat
-        harum libero natus numquam quae, sapiente tempora, ut vel velit vitae
-        voluptates.
-      </p>
+  }
 
-      <button onClick={onClickHandler}>to Sign Up Page</button>
-    </Layout.Page>
+  // обновляем стэйт формы при изменении инпутов
+  function formChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setFormVal({ ...formVal, [event.target.name]: event.target.value });
+  }
+
+  function onSubmitHandler(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (hasError) return;
+    if (!formVal.login || !formVal.password) return;
+
+    sendRequest(
+      {
+        url: `${authUrl}/signin`,
+        method: "POST",
+        body: formVal,
+      },
+      applyData,
+    );
+  }
+
+  function applyData(data?: any) {
+    if (data) {
+      console.log(data);
+    }
+
+    sendRequest({ url: `${authUrl}/user` });
+    // navigate(routes.game) или navigate(routes.forum) ???
+  }
+
+  return (
+    // убрал layout.page т.к. с ним аполучается 3 х вложенность
+    // main->page->container, оно нам необходимо?
+    <form className={style.form} onSubmit={onSubmitHandler}>
+      <h1>Please auth</h1>
+
+      <div className={style["form__inputs-wrapper"]}>
+        <ExoticInput
+          name="login"
+          label="Login"
+          required={true}
+          onChange={formChangeHandler}
+          onError={val => setHasError(val)}
+        />
+        <ExoticInput
+          name="password"
+          label="Password"
+          type="password"
+          required={true}
+          onChange={formChangeHandler}
+          onError={val => setHasError(val)}
+        />
+      </div>
+
+      <div className={style["form__btns-wrapper"]}>
+        <FlatButton name="Login" type="submit" />
+        <FlatButton name="Signup" onClick={onClickHandler} transparent={true} />
+      </div>
+    </form>
   );
-};
+}
 
 export default LoginPage;
