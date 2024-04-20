8b8a31b1f2558e706f1d50f41bf5dc85de01188d
Julius Gerba <juliusgerba@gmail.com>
2024-04-20 09:45:27 +0300

    feat: add fetch hook

---
+++ packages/client/src/hooks/useFetch.tsx
@@ -0,0 +1,58 @@
+type configType = {
+  url: string;
+  method?: string;
+  body?: object;
+};
+
+type responseType = {
+  [key: string]: Record<string, string | { [key: string]: string } | number>;
+};
+
+function useFetch() {
+  // const dispatch = useDispatch();
+
+  function sendRequest(
+    config: configType,
+    applyData?: (data: responseType) => void,
+  ) {
+    // включить индикатор загрузки
+    // dispatch(notifyActions.applyLoading());
+
+    // очитсить сообщения об ошибке
+    // dispatch(notifyActions.clearError());
+
+    fetch(config.url, {
+      method: config.method ? config.method : "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: config.body ? JSON.stringify(config.body) : null,
+    })
+      .then(async response => {
+        console.log(response);
+        const data = (await response.json()) as responseType;
+
+        if (!response.ok) {
+          throw new Error(`${response.status} ${data.reason}`);
+        }
+
+        if (applyData) {
+          applyData(data);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        // добавляем сообщение об ошибке
+        // dispatch(
+        //   notifyActions.applyError(error.message || "Something went wrong"),
+        // );
+      })
+      .finally(() => {
+        // выключить индикатор загрузки
+        // dispatch(notifyActions.clearLoading());
+      });
+  }
+  return sendRequest;
+}
+
+export default useFetch;
