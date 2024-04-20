3ef012d5432c9ec3b208e1f2a20d29721ac6e42f
Julius Gerba <juliusgerba@gmail.com>
2024-04-20 10:08:26 +0300

    feat: add dangerous flat btn

--- packages/client/src/ui/flat-button/index.tsx
+++ packages/client/src/ui/flat-button/index.tsx
@@ -5,13 +5,14 @@ type buttonType = {
   type?: "submit" | "reset";
   disabled?: boolean;
   transparent?: boolean;
+  dangerous?: boolean;
   onClick?: (event: React.MouseEvent) => void;
 };
 
 function FlatButton(props: buttonType) {
   return (
     <button
-      className={`${style["button"]} ${props.transparent ? style["button--transparent"] : ""}`}
+      className={`${style["button"]} ${props.transparent ? style["button--transparent"] : props.dangerous ? style["button--dangerous"] : ""}`}
       name={props.name}
       type={props.type ? props.type : "button"}
       disabled={props.disabled}
