aff3e569299b177f1347aa3935bfed5f327cbe92
Julius Gerba <juliusgerba@gmail.com>
2024-04-24 22:29:41 +0300

    style: upd width styles; add header classnames

--- packages/client/src/ui/title/index.tsx
+++ packages/client/src/ui/title/index.tsx
@@ -1,16 +1,19 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import cn from "classnames";
 
 import style from "./style.module.scss";
 
 export type TProps = {
   title: string;
-};
-const H1: FC<TProps> = ({ title }) => {
-  return <h1 className={style.title}>{title}</h1>;
+  className?: string;
 };
 
-const H2: FC<TProps> = ({ title }) => {
-  return <h2 className={style.title}>{title}</h2>;
+const H1: FC<TProps> = ({ title, className }) => {
+  return <h1 className={cn(style.title, className)}>{title}</h1>;
+};
+
+const H2: FC<TProps> = ({ title, className }) => {
+  return <h2 className={cn(style.title, className)}>{title}</h2>;
 };
 
 const Title = {
