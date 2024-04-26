11c76cc6c34068d81193667da5dc14349da15d8c
sergrage <elenergetica87@mail.ru>
2024-04-26 17:49:05 +0300

    game over page

--- packages/client/src/pages/gameOver/index.tsx
+++ packages/client/src/pages/gameOver/index.tsx
@@ -3,14 +3,21 @@ import React, { FC } from "react";
 import style from "./style.module.scss";
 import Title from "@/ui/title";
 import Layout from "@/components/Layout";
+import Button from "@/ui/button";
 
 const GameOverPage: FC = () => {
   return (
     <Layout.Page>
       <div className={style.wrapper}>
         <h1 className={style.title}>GAME OVER</h1>
-        <p className={style.score}>SCORE: 500</p>
-        <button>Start Again?</button>
+        <p className={style.score}>YOUR SCORE: 500</p>
+
+        <div className="">
+          <img src="/src/assets/img/lightning.png" alt="" />
+          <img src="/src/assets/img/lightning.png" alt="" />
+          <img src="/src/assets/img/lightningDark.png" alt="" />
+        </div>
+        <Button.Flat name="Start Again?" formBtn={true} formBtnRed={true} />
       </div>
     </Layout.Page>
   );
