491afc43d90276d24bad493a2a1841523dbfe656
Julius Gerba <juliusgerba@gmail.com>
2024-05-01 12:13:17 +0300

    refactor: remove react import

--- packages/client/src/pages/login/index.tsx
+++ packages/client/src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC, useEffect, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 import { routes } from "@/pages/routes";
