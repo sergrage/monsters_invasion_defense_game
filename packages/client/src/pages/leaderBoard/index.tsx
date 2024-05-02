3b3b900862962fa9928221940f8576fd91c66228
BJuice1984 <beetle_juice84@mail.ru>
2024-05-02 23:04:49 +0300

    fix(leaderborad): fix layout and styles

--- packages/client/src/pages/leaderBoard/index.tsx
+++ packages/client/src/pages/leaderBoard/index.tsx
@@ -4,23 +4,58 @@ import style from "./style.module.scss";
 import Image from "@/ui/image";
 import Layout from "@/components/layout";
 import zombieAlarm from "@/assets/img/zombieAlarm.png";
+import tempData from "./temp_data";
 
 import cn from "classnames";
 
 const LeaderBoardPage: FC = () => {
   return (
     <Layout.Page>
-      <div className={style.hero}>
+      <section className={style.leaderboard}>
         <div className={cn(style.wrapper)}>
           <h2 className={cn(style.title)}>Leaderboard</h2>
-
           <Image
             className={style.zombieAlarm}
             src={zombieAlarm}
             alt="zombieAlarm"
-          ></Image>
+          />
         </div>
-      </div>
+        <div className={style.tableWrapper}>
+          <table className={style.table}>
+            <thead>
+              <tr>
+                <th scope="col">Rank</th>
+                <th scope="col">Kills</th>
+                <th scope="col">User</th>
+                <th scope="col">Total money</th>
+                <th scope="col">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tempData
+                .sort((a, b) => a.rank - b.rank)
+                .map(item => (
+                  <tr onClick={() => console.log("jmi")} key={item.id}>
+                    <td className={style.boldText}>{item.rank}</td>
+                    <td>{item.zombieKills}</td>
+                    <td>
+                      <div className={style.user}>
+                        <img
+                          src={item.user.avatar}
+                          className={style.avatar}
+                          alt="RRR! AVATAR!"
+                        />
+                        <p>{item.user.login}</p>
+                      </div>
+                    </td>
+                    <td>{item.earnMoney}</td>
+                    <td>{item.date}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </Layout.Page>
   );
 };
---
+++ packages/client/src/pages/leaderBoard/temp_data.ts
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    user: {
+      name: "Sergrage",
+      login: "elenergetica87",
+      avatar: "/src/assets/img/user1.png",
+    },
+    rank: 1,
+    zombieKills: 134,
+    earnMoney: 2650,
+    date: "02.05.2024",
+  },
+  {
+    id: 2,
+    user: {
+      name: "UserName2",
+      login: "zombiKiller226",
+      avatar: "/src/assets/img/user2.png",
+    },
+    rank: 3,
+    zombieKills: 112,
+    earnMoney: 1650,
+    date: "03.05.2024",
+  },
+  {
+    id: 3,
+    user: {
+      name: "UserName3",
+      login: "-^ ^- sushilover ~(_8^(|)",
+      avatar: "/src/assets/img/user3.png",
+    },
+    rank: 2,
+    zombieKills: 120,
+    earnMoney: 1900,
+    date: "03.05.2024",
+  },
+  {
+    id: 4,
+    user: {
+      name: "UserName4",
+      login: "Раджа",
+      avatar: "/src/assets/img/",
+    },
+    rank: 4,
+    zombieKills: 20,
+    earnMoney: 200,
+    date: "04.05.2024",
+  },
+];
