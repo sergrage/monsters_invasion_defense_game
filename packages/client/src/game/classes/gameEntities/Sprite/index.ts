1996b6141fcdf53c13c88d5f3b64fd01208c3535
Julius Gerba <juliusgerba@gmail.com>
2024-05-04 15:53:04 +0300

    feat: upd enemy args & types

--- packages/client/src/game/classes/gameEntities/Sprite/index.ts
+++ packages/client/src/game/classes/gameEntities/Sprite/index.ts
@@ -1,33 +1,23 @@
-import { Position, Frames, Offset } from "@/game/interfaces";
+import { ISprite } from "@/game/interfaces";
 
 class Sprite {
-  position;
-  canvas;
-  image;
-  frames;
-  offset;
-  c;
+  position; // stores the x and y coordinates of the sprite on the canvas
+  canvas; // reference to the HTML canvas element where the sprite will be drawn
+  image; // an Image object holding the sprite's image data
+  frames; // information about the animation frames
+  offset; // offset for the sprite's position
+  c; // canvas rendering context used for drawing
 
   constructor({
-    position = { x: 0, y: 0 },
+    position,
     canvas,
     imageSrc,
-    frames = {
-      max: 1,
-      current: 0,
-      elapsed: 0,
-      hold: 0,
-    },
+    // set in each entity
+    frames,
+    // set in each entity
     offset = { x: 0, y: 0 },
     c,
-  }: {
-    position?: Position;
-    canvas: HTMLCanvasElement;
-    imageSrc: string;
-    frames?: Frames;
-    offset?: Offset;
-    c: CanvasRenderingContext2D;
-  }) {
+  }: ISprite) {
     this.position = position;
     this.canvas = canvas;
     this.image = new Image();
@@ -43,35 +33,42 @@ class Sprite {
   }
 
   draw(): void {
+    // calculate the width of a single frame
     const cropWidth = this.image.width / this.frames.max;
+
+    // define the cropping region for the current frame
     const crop = {
       position: {
         // возможно прописать условие на случай отсутсвия свойства или изменит интерфейс...
-        x: cropWidth * this.frames.current!,
-        y: 0,
+        x: cropWidth * this.frames.current!, // x-coordinate of the current frame within the image
+        y: 0, // assuming frames are arranged in a single row
       },
-      width: cropWidth,
-      height: this.image.height,
+      width: cropWidth, // width of a single frame
+      height: this.image.height, // full height of the image
     };
     this.c.drawImage(
       this.image,
-      crop.position.x,
-      crop.position.y,
+      crop.position.x, // source x-coordinate within the image
+      crop.position.y, // source y-coordinate within the image
       crop.width,
       crop.height,
-      this.position.x + this.offset.x,
-      this.position.y + this.offset.y,
+      this.position.x + this.offset.x, // destination x-coordinate on the canvas with offset
+      this.position.y + this.offset.y, // destination y-coordinate on the canvas with offset
       crop.width,
       crop.height,
     );
   }
 
+  // update the animation frame
   update(): void {
-    // responsible for animation
     // возможно прописать условие на случай отсутсвия свойств или изменит интерфейс...
     this.frames.elapsed!++;
+
+    // check if it's time to advance to the next frame
     if (this.frames.elapsed! % this.frames.hold! === 0) {
       this.frames.current!++;
+
+      // check if the animation has ended => return to the first frame
       if (this.frames.current! >= this.frames.max) {
         this.frames.current = 0;
       }
--- packages/client/src/game/interfaces/index.ts
+++ packages/client/src/game/interfaces/index.ts
@@ -14,3 +14,32 @@ export interface Offset {
   x: number;
   y: number;
 }
+
+export interface IContext {
+  position: Position;
+  canvas: HTMLCanvasElement;
+  c: CanvasRenderingContext2D;
+}
+
+export interface IFrame {
+  imageSrc: string;
+  frames: Frames;
+  c: CanvasRenderingContext2D;
+}
+
+export interface IEntityParams {
+  width: number;
+  height: number;
+  waypointIndex: number;
+  radius: number;
+  health: number;
+  velocity: Position;
+}
+
+export interface IEntitySprite extends IContext, IFrame {
+  entityParams: IEntityParams;
+}
+
+export interface ISprite extends IContext, IFrame {
+  offset?: Offset;
+}
