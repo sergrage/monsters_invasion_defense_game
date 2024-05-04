0d2eb3f597db7ff98e1efaabd3e054fd12ce7b8b
Julius Gerba <juliusgerba@gmail.com>
2024-05-04 15:54:29 +0300

    refactor: remove redundant files, add coments

--- packages/client/src/game/classes/creational/tilesGenerator.ts
+++ packages/client/src/game/classes/creational/tilesGenerator.ts
@@ -27,16 +27,21 @@ class TilesGenerator {
 
   generatePlacementTiles(): PlacementTile[] {
     const placementTilesData2D: number[][] = [];
+
+    // Divide array into 1x20 rows
     for (let i = 0; i < this.placementTilesData.length; i += 20) {
       placementTilesData2D.push(this.placementTilesData.slice(i, i + 20));
     }
 
     const placementTiles: PlacementTile[] = [];
+
+    // Generate placement tiles
     placementTilesData2D.forEach((row, y) => {
       row.forEach((symbol, x) => {
         if (symbol === 14) {
           placementTiles.push(
             new this.PlacementTile({
+              // tile size
               position: {
                 x: x * 64,
                 y: y * 64,
