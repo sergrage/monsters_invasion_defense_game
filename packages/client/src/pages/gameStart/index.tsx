b8adaabfeb0f902f214dda1bae0fae043cd36e08
sergrage <elenergetica87@mail.ru>
2024-05-06 18:20:02 +0300

    game start fix

--- packages/client/src/pages/gameStart/index.tsx
+++ packages/client/src/pages/gameStart/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import style from "./style.module.scss";
 
