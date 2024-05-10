ff47f463d92ff0bc56e7ad9ac7344e30a6b72b46
BJuice1984 <beetle_juice84@mail.ru>
2024-05-10 11:24:35 +0300

    feat(store, hooks): initial commit

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,1 +1,8 @@
-export {};
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
