3eab59b8461ecbde30bf970aa2ae4052aeb8233b
Julius Gerba <juliusgerba@gmail.com>
2024-05-12 09:35:06 +0300

    refactor: upd towers data

---
+++ packages/client/src/game/mocks/towersData/archerTower.ts
@@ -0,0 +1,51 @@
+import towersPreview from "@/assets/img/towers/towersPreview";
+import shooterImgs from "@/assets/img/towers/archerTower/archer/index";
+import towerImgs from "@/assets/img/towers/archerTower/tower/index";
+import projectileImgs from "@/assets/img/projectiles/index";
+
+import { ITowerParams } from "@/game/interfaces";
+
+const archerTower: ITowerParams = {
+  title: "Archer tower",
+  preview: towersPreview.archer,
+  width: 144,
+  height: 144,
+  radius: 250,
+  speed: 3,
+  price: 50,
+  upgradePrice: 25,
+  imgs: shooterImgs,
+
+  offset: {
+    x: -40,
+    y: -85,
+  },
+
+  frames: {
+    max: 15,
+    current: 0,
+    elapsed: 0,
+    hold: 3,
+  },
+
+  extraParams: {
+    width: 131,
+    height: 153,
+    towerImgs: towerImgs,
+    offset: {
+      x: -33,
+      y: -68,
+    },
+  },
+
+  projectile: {
+    offset: {
+      x: 23,
+      y: 100,
+    },
+    imageSrc: projectileImgs.arrow,
+    needRotation: true,
+  },
+};
+
+export default archerTower;
---
+++ packages/client/src/game/mocks/towersData/teslaTower.ts
@@ -0,0 +1,40 @@
+import towersPreview from "@/assets/img/towers/towersPreview";
+import imgs from "@/assets/img/towers/teslaTower/index";
+import projectileImgs from "@/assets/img/projectiles/index";
+
+import { ITowerParams } from "@/game/interfaces";
+
+const teslaTower: ITowerParams = {
+  title: "Tesla tower",
+  preview: towersPreview.tesla,
+  width: 236,
+  height: 335,
+  radius: 250,
+  speed: 3,
+  price: 60,
+  upgradePrice: 25,
+  imgs: imgs,
+
+  offset: {
+    x: -80,
+    y: -220,
+  },
+
+  frames: {
+    max: 10,
+    current: 0,
+    elapsed: 0,
+    hold: 3,
+  },
+
+  projectile: {
+    offset: {
+      x: 105,
+      y: 310,
+    },
+    imageSrc: projectileImgs.lighter,
+    needRotation: false,
+  },
+};
+
+export default teslaTower;
