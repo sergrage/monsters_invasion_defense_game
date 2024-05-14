f74bf3f5f54216afcc628f3944aabaa9ea692ede
Julius Gerba <juliusgerba@gmail.com>
2024-05-14 22:20:49 +0300

    feat: upd useAuth logic, add updState func

--- packages/client/src/hooks/useAuth.tsx
+++ packages/client/src/hooks/useAuth.tsx
@@ -1,21 +1,47 @@
 import { useEffect, useState } from "react";
-import useFetch from "./useFetch";
-
 import { authUrl } from "@/endpoints/apiUrl";
+
+type responseType = {
+  [key: string]: Record<string, string | { [key: string]: string } | number>;
+};
 
 const useAuth = () => {
   const [isAuth, setIsAuth] = useState(false);
-  const sendRequest = useFetch();
 
   useEffect(() => {
-    sendRequest({ url: `${authUrl}/user` }, applyData);
+    updateAuth();
   }, []);
 
-  const applyData = () => {
-    setIsAuth(true);
+  const updateAuth = () => {
+    fetch(`${authUrl}/user`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then(async response => {
+        const data: responseType = await response.json();
+
+        if (!response.ok) {
+          throw new Error(`${response.status} ${data.reason}`);
+        }
+
+        setIsAuth(true);
+      })
+      .catch(error => {
+        setIsAuth(false);
+        // добавляем сообщение об ошибке
+        // dispatch(
+        //   notifyActions.applyError(error.message || "Something went wrong"),
+        // );
+      })
+      .finally(() => {
+        // выключить индикатор загрузки
+        // dispatch(notifyActions.clearLoading());
+      });
   };
 
-  return isAuth;
+  return { isAuth, updateAuth };
 };
 
 export default useAuth;
