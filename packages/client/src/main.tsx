f7920ec92a404b2966213fd181cda2789a2c1ae9
Nick Yushenko <nickita.yushchenko@prodamus.ru>
2024-05-19 21:51:18 +0300

    upd

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,6 +5,7 @@ import App from "@/App";
 import "@/assets/styles/main.scss";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import ErrorBoundary from "@/components/errorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
