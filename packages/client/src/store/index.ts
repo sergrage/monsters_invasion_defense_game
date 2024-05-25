2211a8dbf090811c934bdb43d876e26d36b25c17
Julius Gerba <juliusgerba@gmail.com>
2024-05-25 12:12:42 +0300

    refactor: remove redundant code

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,16 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { leaderboardSlice } from "./leaderboard/reducer";
-import userReducer from "./user/reducer";
-
-import notifyReducer from "./notification/reducer";
 import { errorSlice } from "./error/reducer";
+import userReducer from "./user/reducer";
 
 export const store = configureStore({
   reducer: combineReducers({
     error: errorSlice.reducer,
     leaderboard: leaderboardSlice.reducer,
     user: userReducer,
-    notify: notifyReducer,
   }),
 });
 
