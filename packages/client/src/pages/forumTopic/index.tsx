a95b96a5e8017c987693e0de09bb4f0fcb6f4c43
Anton Wolynski <50911673+Volynskii@users.noreply.github.com>
2024-06-19 16:32:31 +0300

    emoji for forum;icon to open and close; close emoji picker when click outside;

--- packages/client/src/pages/forumTopic/index.tsx
+++ packages/client/src/pages/forumTopic/index.tsx
@@ -1,13 +1,95 @@
-import React, { FC } from "react";
-
+import React, { FC, useState, useRef, useEffect } from "react";
 import style from "./style.module.scss";
 import Title from "@/ui/title";
-
 import { useParams } from "react-router";
-
 import temp_data from "@/pages/forumTopic/temp_data";
 import Button from "@/ui/button";
 import Layout from "@/components/layout";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+
+interface User {
+  name: string;
+  avatar: string;
+}
+
+interface ForumTopicMessageProps {
+  item: {
+    id: number;
+    date: string;
+    user: User;
+    message: string;
+  };
+}
+
+const ForumTopicMessage: FC<ForumTopicMessageProps> = ({ item }) => {
+  const [showPicker, setShowPicker] = useState(false);
+  const [selectedEmojis, setSelectedEmojis] = useState<string[]>([]);
+  const pickerRef = useRef<HTMLDivElement>(null);
+
+  const togglePicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const handleEmojiSelect = (emoji: any) => {
+    setSelectedEmojis([...selectedEmojis, emoji.native]);
+    setShowPicker(false);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      pickerRef.current &&
+      !pickerRef.current.contains(event.target as Node)
+    ) {
+      setShowPicker(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showPicker) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showPicker]);
+
+  return (
+    <div className={style.message} key={item.id}>
+      <div className={style.date}>
+        <span>{item.date}</span>
+        <span>#{item.id}</span>
+      </div>
+      <div className={style.body}>
+        <div className={style.user}>
+          <div className={style.name}>{item.user.name}</div>
+          <div className={style.avatar}>
+            <img src={item.user.avatar} alt="avatar" />
+          </div>
+        </div>
+        <div className={style.text}>
+          <p>{item.message}</p>
+          <div className={style.emojiContainer}>
+            <div className={style.selectedEmojis}>
+              {selectedEmojis.map((emoji, index) => (
+                <span key={index}>{emoji}</span>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      {showPicker && (
+        <div className={style.emojiPicker} ref={pickerRef}>
+          <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+        </div>
+      )}
+      <button className={style.emojiButton} onClick={togglePicker} />
+    </div>
+  );
+};
 
 const ForumTopics: FC = () => {
   const params = useParams();
@@ -15,8 +97,8 @@ const ForumTopics: FC = () => {
   // TODO get message by topic id
   const onSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form as HTMLFormElement);
+    const form = event.currentTarget as HTMLFormElement;
+    const formData = new FormData(form);
     const formJson = Object.fromEntries(formData.entries());
   };
 
@@ -30,23 +112,7 @@ const ForumTopics: FC = () => {
       </div>
 
       {temp_data.map(item => (
-        <div className={style.message} key={item.id}>
-          <div className={style.date}>
-            <span>{item.date}</span>
-            <span>#{item.id}</span>
-          </div>
-          <div className={style.body}>
-            <div className={style.user}>
-              <div className={style.name}>{item.user.name}</div>
-              <div className={style.avatar}>
-                <img src={item.user.avatar} alt="avatar" />
-              </div>
-            </div>
-            <div className={style.text}>
-              <p>{item.message}</p>
-            </div>
-          </div>
-        </div>
+        <ForumTopicMessage item={item} key={item.id} />
       ))}
 
       <form onSubmit={onSubmitHandler}>
