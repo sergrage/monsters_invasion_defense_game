e5ebda7a07d75d3aba3ed49e04f33ab54c439267
Julius Gerba <juliusgerba@gmail.com>
2024-06-25 20:09:00 +0300

    fix: change proxy position

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -25,15 +25,6 @@ app.use(
   }),
 );
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const port = Number(process.env.SERVER_PORT) || 3001;
-
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
-});
-
 app.use(
   "/api/v2",
   createProxyMiddleware({
@@ -75,6 +66,15 @@ app.use(
     logger: console,
   } as IOptions),
 );
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = Number(process.env.SERVER_PORT) || 3001;
+
+app.listen(port, () => {
+  console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
+});
 
 app.use("/api", routes);
 
