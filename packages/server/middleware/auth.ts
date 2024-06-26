4154c9bc7d752345df00967c20294fb41e4100ed
Julius Gerba <juliusgerba@gmail.com>
2024-06-26 21:26:35 +0300

    feat: add user data to auth requests

--- packages/server/middleware/auth.ts
+++ packages/server/middleware/auth.ts
@@ -1,6 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import fetch from "node-fetch";
 
+type User = {
+  id: string;
+  first_name: string;
+  second_name: string;
+  display_name: string;
+  phone: string;
+  login: string;
+  email: string;
+  avatar: string;
+};
+
+// add user data to request
+declare global {
+  namespace Express {
+    interface Request {
+      userData: User;
+    }
+  }
+}
+
 // check auth, if user is authorized => continue
 export const auth = async (
   req: Request,
@@ -23,6 +43,10 @@ export const auth = async (
     });
 
     if (response.ok) {
+      // add user data to request
+      const data = await response.json();
+      req.userData = data;
+
       next();
     } else {
       throw new Error("Unauthorized");
