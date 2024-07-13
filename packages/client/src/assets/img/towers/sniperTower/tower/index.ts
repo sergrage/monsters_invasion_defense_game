595342cbcf3eb5e4d6e584333c04e041089887fd
BJuice1984 <beetle_juice84@mail.ru>
2024-07-13 22:02:10 +0300

    feat(sniperTower, archerTower): add new sprites

--- packages/client/src/assets/img/towers/sniperTower/tower/index.ts
+++ packages/client/src/assets/img/towers/sniperTower/tower/index.ts
@@ -1,3 +1,7 @@
 import img1 from "./1.png";
+import img2 from "./2.png";
+import img3 from "./3.png";
+import img4 from "./4.png";
+import img5 from "./5.png";
 
-export default [img1];
+export default [img1, img2, img3, img4, img5];
